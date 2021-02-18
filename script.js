1ee9a978557e67661f4e856e901b0d75d6515556
SamirAllaham <samir.allaham@ocsbstudent.ca>
2021-02-18 18:11:39 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Connect "Guess" button to "random" function
+document.getElementById('button').addEventListener('click', comparison)
+let userInput = 0
+
+function comparison () {
+  //get user input and convert to an integer
+  userInput = document.getElementById('input').value
+  userInput = parseInt(userInput)
+
+  //Check if userInput is less than 0
+  if (userInput < 0) {
+    document.getElementById('answer').innerHTML = 'Negative'
+  }
+}
