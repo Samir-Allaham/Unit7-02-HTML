a130a42eb0bcc8bccc0f2987a476d02b17203b55
SamirAllaham <samir.allaham@ocsbstudent.ca>
2021-02-18 18:24:50 +0000

    spaces

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ document.getElementById('button').addEventListener('click', comparison)
 let userInput = 0
 
 function comparison () {
-  //get user input and convert to an integer
+  // get user input and convert to an integer
   userInput = document.getElementById('input').value
   userInput = parseInt(userInput)
 
-  //Check if userInput is less than 0
+  // Check if userInput is less than 0
   if (userInput < 0) {
     document.getElementById('answer').innerHTML = 'Negative'
+  }
+  else{
+    document.getElementById('answer').innerHTML = 'Positive'
   }
 }
